816b3d8e710fc65216c6fcf1242e434dd16ac8a3
James <James@voxdsp.com>
2019-10-19 12:06:41 +0100

    general

--- bin/build-vfui-Desktop_Qt_5_12_2_GCC_64bit-Release/ui_mainwindow.h
+++ bin/build-vfui-Desktop_Qt_5_12_2_GCC_64bit-Release/ui_mainwindow.h
@@ -38,8 +38,6 @@ public:
     QLabel *label_10;
     QPushButton *send_trans;
     QLabel *label_12;
-    QLabel *label_13;
-    QFrame *line_3;
     QPushButton *login;
     QFrame *line_4;
     QLabel *label_17;
@@ -53,6 +51,9 @@ public:
     QPushButton *view;
     QTextEdit *explore_result;
     QPushButton *privsend;
+    QFrame *line_3;
+    QFrame *line_8;
+    QLabel *label_13;
     QStatusBar *statusbar;
 
     void setupUi(QMainWindow *MainWindow)
@@ -60,7 +61,7 @@ public:
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
         MainWindow->setEnabled(true);
-        MainWindow->resize(592, 686);
+        MainWindow->resize(592, 666);
         QIcon icon;
         icon.addFile(QString::fromUtf8(":/new/prefix1/favicon"), QSize(), QIcon::Normal, QIcon::Off);
         MainWindow->setWindowIcon(icon);
@@ -76,7 +77,7 @@ public:
         newkey->setFont(font);
         explore_address = new QLineEdit(centralwidget);
         explore_address->setObjectName(QString::fromUtf8("explore_address"));
-        explore_address->setGeometry(QRect(10, 230, 461, 31));
+        explore_address->setGeometry(QRect(10, 210, 461, 31));
         loginpriv = new QLineEdit(centralwidget);
         loginpriv->setObjectName(QString::fromUtf8("loginpriv"));
         loginpriv->setGeometry(QRect(70, 10, 441, 31));
@@ -112,15 +113,6 @@ public:
         label_12->setObjectName(QString::fromUtf8("label_12"));
         label_12->setGeometry(QRect(320, 100, 131, 20));
         label_12->setFont(font1);
-        label_13 = new QLabel(centralwidget);
-        label_13->setObjectName(QString::fromUtf8("label_13"));
-        label_13->setGeometry(QRect(30, 200, 251, 20));
-        label_13->setFont(font1);
-        line_3 = new QFrame(centralwidget);
-        line_3->setObjectName(QString::fromUtf8("line_3"));
-        line_3->setGeometry(QRect(10, 180, 571, 16));
-        line_3->setFrameShape(QFrame::HLine);
-        line_3->setFrameShadow(QFrame::Sunken);
         login = new QPushButton(centralwidget);
         login->setObjectName(QString::fromUtf8("login"));
         login->setGeometry(QRect(520, 10, 61, 31));
@@ -139,46 +131,60 @@ public:
         balance->setGeometry(QRect(170, 70, 411, 20));
         line_5 = new QFrame(centralwidget);
         line_5->setObjectName(QString::fromUtf8("line_5"));
-        line_5->setGeometry(QRect(10, 630, 571, 16));
+        line_5->setGeometry(QRect(10, 610, 571, 16));
         line_5->setFrameShape(QFrame::HLine);
         line_5->setFrameShadow(QFrame::Sunken);
         label_14 = new QLabel(centralwidget);
         label_14->setObjectName(QString::fromUtf8("label_14"));
-        label_14->setGeometry(QRect(40, 650, 111, 20));
+        label_14->setGeometry(QRect(40, 630, 111, 20));
         label_14->setTextFormat(Qt::RichText);
         label_14->setOpenExternalLinks(true);
         label_15 = new QLabel(centralwidget);
         label_15->setObjectName(QString::fromUtf8("label_15"));
-        label_15->setGeometry(QRect(290, 650, 121, 20));
+        label_15->setGeometry(QRect(290, 630, 121, 20));
         label_15->setTextFormat(Qt::RichText);
         label_15->setOpenExternalLinks(true);
         label_16 = new QLabel(centralwidget);
         label_16->setObjectName(QString::fromUtf8("label_16"));
-        label_16->setGeometry(QRect(160, 650, 121, 20));
+        label_16->setGeometry(QRect(160, 630, 121, 20));
         label_16->setTextFormat(Qt::RichText);
         label_16->setOpenExternalLinks(true);
         label_19 = new QLabel(centralwidget);
         label_19->setObjectName(QString::fromUtf8("label_19"));
-        label_19->setGeometry(QRect(420, 650, 71, 20));
+        label_19->setGeometry(QRect(420, 630, 71, 20));
         label_19->setTextFormat(Qt::RichText);
         label_19->setOpenExternalLinks(true);
         label_20 = new QLabel(centralwidget);
         label_20->setObjectName(QString::fromUtf8("label_20"));
-        label_20->setGeometry(QRect(500, 650, 61, 20));
+        label_20->setGeometry(QRect(500, 630, 61, 20));
         label_20->setTextFormat(Qt::RichText);
         label_20->setOpenExternalLinks(true);
         view = new QPushButton(centralwidget);
         view->setObjectName(QString::fromUtf8("view"));
-        view->setGeometry(QRect(470, 230, 111, 31));
+        view->setGeometry(QRect(470, 210, 111, 31));
         view->setFont(font);
         explore_result = new QTextEdit(centralwidget);
         explore_result->setObjectName(QString::fromUtf8("explore_result"));
-        explore_result->setGeometry(QRect(10, 260, 571, 361));
+        explore_result->setGeometry(QRect(10, 240, 571, 361));
         explore_result->setReadOnly(true);
         privsend = new QPushButton(centralwidget);
         privsend->setObjectName(QString::fromUtf8("privsend"));
         privsend->setGeometry(QRect(530, 140, 51, 31));
         privsend->setFont(font1);
+        line_3 = new QFrame(centralwidget);
+        line_3->setObjectName(QString::fromUtf8("line_3"));
+        line_3->setGeometry(QRect(10, 180, 41, 16));
+        line_3->setFrameShape(QFrame::HLine);
+        line_3->setFrameShadow(QFrame::Sunken);
+        line_8 = new QFrame(centralwidget);
+        line_8->setObjectName(QString::fromUtf8("line_8"));
+        line_8->setGeometry(QRect(220, 180, 361, 16));
+        line_8->setFrameShape(QFrame::HLine);
+        line_8->setFrameShadow(QFrame::Sunken);
+        label_13 = new QLabel(centralwidget);
+        label_13->setObjectName(QString::fromUtf8("label_13"));
+        label_13->setGeometry(QRect(60, 180, 251, 20));
+        label_13->setFont(font1);
         MainWindow->setCentralWidget(centralwidget);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName(QString::fromUtf8("statusbar"));
@@ -202,7 +208,6 @@ public:
         label_10->setText(QApplication::translate("MainWindow", "To Address:", nullptr));
         send_trans->setText(QApplication::translate("MainWindow", "Send", nullptr));
         label_12->setText(QApplication::translate("MainWindow", "Send Transaction", nullptr));
-        label_13->setText(QApplication::translate("MainWindow", "Transaction Explorer", nullptr));
         login->setText(QApplication::translate("MainWindow", "Login", nullptr));
         label_17->setText(QApplication::translate("MainWindow", "Account Balance:", nullptr));
         balance->setText(QApplication::translate("MainWindow", "0 VFC", nullptr));
@@ -213,6 +218,7 @@ public:
         label_20->setText(QApplication::translate("MainWindow", "<a href=\"https://discord.gg/VFa4A5v\">Discord</a>", nullptr));
         view->setText(QApplication::translate("MainWindow", "Load", nullptr));
         privsend->setText(QApplication::translate("MainWindow", "Priv", nullptr));
+        label_13->setText(QApplication::translate("MainWindow", "Transaction Explorer", nullptr));
     } // retranslateUi
 
 };
